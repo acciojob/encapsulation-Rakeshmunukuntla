12ed4719534ec7aba0e1b9c11f87db9370b831df
Rakesh Munukuntla <rakesh9490434912@gmail.com>
2024-07-09 16:16:15 +0530

    assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
-public class Main {
-  
+public class Main
+{
+  public static void  main(String[] args)
+  {
+    RWOnly rwOnly=new  RWOnly();
+   // rwOnly.name="rakesh";
+   // System.out.println(rwOnly.name);
+    rwOnly.setName("rakesh");
+    System.out.println(rwOnly.getName());
+  }
 }
